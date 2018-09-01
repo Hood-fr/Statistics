49abb4629b39cf4477ada4265521814c3d867e60
Robin MORET <robin.moret@supaero.org>
2018-09-01 15:55:10 +0200

    Init of Statistics plugin based on version 2.7.a

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+$lang['Howto'] = 'Dieses Plug-In erlaubt es Ihnen die Codes von Google Analytics, PhpMyVisites, etc. auf jede Seite Ihrer Galerie hinzuzufügen';
+$lang['statistics_admin'] = 'Administration der Statistik';
+$lang['statistics_admin_description'] = 'Hier den Code einfügen. ';
+$lang['stat_header'] = 'Fügt den Code in die Kopfzeile ein';
+$lang['stat_tail'] = 'Fügt den Code in die Fußzeile ein';
+$lang['statistics_save_config'] = 'Daten wurden gespeichert';
+$lang['stat_admin'] = 'Schließt den  Administrator aus der Statistik aus.';
+$lang['stat_guest'] = 'Schließt die Gäste aus der Statistik aus.';
+?>
